ff6c33ffaaf6c864d6a17c5cb1790ac9e628772b
alperkyoruk <alperkyoruk@gmail.com>
2024-03-11 16:08:13 +0300

    Rating Application for WEBLAB Projects

--- src/main/java/weblab/rating/business/constants/Messages.java
+++ src/main/java/weblab/rating/business/constants/Messages.java
@@ -1,10 +1,10 @@
 package weblab.rating.business.constants;
 
 public class Messages {
-    public static String websiteCannotBeNull;
-    public static String ratingValueNotValid;
-    public static String ratingAddSuccess;
-    public static String ratingDoesntExist;
-    public static String getRatingsByWebsiteSuccess;
-    public static String getRatingByIdSuccess;
+    public static String websiteCannotBeNull= "Website cannot be null";
+    public static String ratingValueNotValid = "Rating value must be between 0 and 5";
+    public static String ratingAddSuccess = "Rating added successfully";
+    public static String ratingDoesntExist = "Rating doesn't exist";
+    public static String getRatingsByWebsiteSuccess = "Ratings fetched successfully";
+    public static String getRatingByIdSuccess = "Rating fetched successfully";
 }
